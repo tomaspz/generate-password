05ed88d142d5f04d37f1738a941cc1c4c3108e4c
Tomas Perez <tomaspz@gmail.com>
2020-03-05 21:11:38 -0500

    created variables and added some functions

--- script.js
+++ script.js
@@ -5,30 +5,38 @@ var generateBtn = document.querySelector("#generate");
 // The goal is to return something
 
 function generatePassword(){
+  var passwordChar = '';
+  var lowercaseChar = 'abcdefghijklmnopqrstuvwxyz';
+  var uppercaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  // var specialChar = ["!",'"', "#", "$", "%", "&", "'", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", '\', "]", "^", "_", '`', '{', '|', '}', '~'];
+  var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";   // I had to add a backslahs in front of the double quote character
+  var numberChar = '123456789';
+  
   // Prompt user for password length
-  var userInputLength = prompt("Choose a password with 8, 9 or 10 characters (type the number)")
-  // Check to see if user entered a length
-  if (userInputLength === 8 || userInputLength === 9 || userInputLength === 10) {
-
-  } else {
-
-  };
+  var length = getLength();
+  
   // 4 confirms
 
   // Special Characters
-  var userInputSpecialChar = prompt("Do you want to use special characters?");
+  var special = getSpecialChar();
   // Numbers - 10 characters long
 
   // Lowercase
-  var userInputLowercase = prompt("Do you want lowercase characters?");
+  var lower = getLowercase();
+
   // Uppercase
-  var userInputUppercase = prompt("Do you want uppercase characters?");
+  var upper = getUppercase();
+
+  // Number
+  var numeros = getNumbers();
+
   // generate password based on user soecifications, which come from confirmations
+  var randomPassword = Array(length).fill()
   // " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
   // for(var i=0; i<10 ; i++) { --> length user typed in
 
   // }
-  return "something";
+  return passwordChar;
 }
 
 function writePassword() {
@@ -37,7 +45,37 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
+
+function getLength(){
+  var userInputLength = prompt("Choose a password between 8 and 128 characters (type the number)");
+  // Check to see if user entered a length
+  if (userInputLength >= 8 || userInputLength <= 128) {
+    return userInputLength;
+  } else {
+    prompt("Please type a number between 8 and 128!")
+  }
+};
+
+function getSpecialChar(){
+  var userInputSpecialChar = confirm("Do you want to use special characters?");
+  return userInputSpecialChar;
+};
+
+function getLowercase(){
+  var userInputLowercase = confirm("Do you want lowercase characters?");
+  return userInputLowercase;
+};
+
+function getUppercase() {
+  var userInputUppercase = confirm("Do you want uppercase characters?");
+  return userInputUppercase;
+};
+
+function getNumbers() {
+  var userInputNumber = confirm("Do you want to include numbers from 0 to 9?");
+  return userInputNumber;
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
